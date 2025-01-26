b297a13d60c48af506c460082e316f0eedd7a9e8
Christian165293 <christian.bourquin05@gmail.com>
2025-01-26 00:46:03 -0600

    Made slight change to output to better match the professors example output

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -15,46 +15,45 @@ public class App {
         return number * naivePower(number, power - 1);
     }
 
-  public int unoptimizedDCPower(int number, int power) {
-    if (number == 0) {
-      return 0;
+    public int unoptimizedDCPower(int number, int power) {
+        if (number == 0) {
+            return 0;
+        }
+        if (power == 0) {
+            return 1;
+        }
+        if ((power % 2) == 0) {
+            return unoptimizedDCPower(number, power / 2) * unoptimizedDCPower(number, power / 2);
+        } else {
+            return number * unoptimizedDCPower(number, power / 2) * unoptimizedDCPower(number, power / 2);
+        }
     }
-    if (power == 0) {
-      return 1;
-    }
-    if ((power % 2) == 0) {
-      return unoptimizedDCPower(number, power / 2) * unoptimizedDCPower(number, power / 2);
-    } else {
-      return number * unoptimizedDCPower(number, power / 2) * unoptimizedDCPower(number, power / 2);
-    }
-  }
 
-  public int optimizedDCPower(int number, int power) {
-    if (number == 0) {
-      return 0;
+    public int optimizedDCPower(int number, int power) {
+        if (number == 0) {
+            return 0;
+        }
+        if (power == 0) {
+            return 1;
+        }
+        int temp = optimizedDCPower(number, power / 2);
+        if ((power % 2) == 0) {
+            return temp * temp;
+        } else {
+            return number * temp * temp;
+        }
     }
-    if (power == 0) {
-      return 1;
-    }
-    int temp = optimizedDCPower(number, power / 2);
-    if ((power % 2) == 0) {
-      return temp * temp;
-    } else {
-      return number * temp * temp;
-    }
-  }
 
-  public static void main(String[] args) {
-    App app = new App();
-    System.out.println(new App().getGreeting());
-    for (int i = 0; i < 10; i++) {
-      int numberInput = (int) (Math.random() * 5);
-      int powerInput = (int) (Math.random() * 5);
-      System.out.println("number input:" + numberInput + "\tpower input:" + powerInput);
-      System.out.println("these are the created methods which should match each other");
-      System.out.println(app.naivePower(numberInput, powerInput));
-      System.out.println(app.unoptimizedDCPower(numberInput, powerInput));
-      System.out.println(app.optimizedDCPower(numberInput, powerInput) +"\n");
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println(new App().getGreeting());
+        for (int i = 0; i < 10; i++) {
+            int numberInput = (int) (Math.random() * 5);
+            int powerInput = (int) (Math.random() * 5);
+            System.out.println("naivePower(" + numberInput + ", " + powerInput + ") == " + app.naivePower(numberInput, powerInput) + ";");
+            System.out.println("unoptimizedDCPower(" + numberInput + ", " + powerInput + ") == " + app.unoptimizedDCPower(numberInput, powerInput) + ";");
+            System.out.println("optimizedDCPower(" + numberInput + ", " + powerInput + ") == " + app.optimizedDCPower(numberInput, powerInput) + ";\n");
+
+        }
     }
-  }
 }
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -1,41 +1,43 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-  @Test
-  void appHasAGreeting() {
-    App classUnderTest = new App();
-    assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-  }
-  @Test
-  public void testNaivePower() {
-    App classUnderTest = new App();
-    assertEquals(1, classUnderTest.naivePower(2, 0), "2^0 should be 1");
-    assertEquals(2, classUnderTest.naivePower(2, 1), "2^1 should be 2");
-    assertEquals(4, classUnderTest.naivePower(2, 2), "2^2 should be 4");
-    assertEquals(8, classUnderTest.naivePower(2, 3), "2^3 should be 8");
-    assertEquals(1024, classUnderTest.naivePower(2, 10), "2^10 should be 1024");
-  }
+    @Test
+    void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
 
-  @Test
-  public void testUnoptimizedDCPower() {
-    App classUnderTest = new App();
-    assertEquals(1, classUnderTest.unoptimizedDCPower(2, 0), "2^0 should be 1");
-    assertEquals(2, classUnderTest.unoptimizedDCPower(2, 1), "2^1 should be 2");
-    assertEquals(4, classUnderTest.unoptimizedDCPower(2, 2), "2^2 should be 4");
-    assertEquals(8, classUnderTest.unoptimizedDCPower(2, 3), "2^3 should be 8");
-    assertEquals(1024, classUnderTest.unoptimizedDCPower(2, 10), "2^10 should be 1024");
-  }
+    @Test
+    public void testNaivePower() {
+        App classUnderTest = new App();
+        assertEquals(1, classUnderTest.naivePower(2, 0), "2^0 should be 1");
+        assertEquals(2, classUnderTest.naivePower(2, 1), "2^1 should be 2");
+        assertEquals(4, classUnderTest.naivePower(2, 2), "2^2 should be 4");
+        assertEquals(8, classUnderTest.naivePower(2, 3), "2^3 should be 8");
+        assertEquals(1024, classUnderTest.naivePower(2, 10), "2^10 should be 1024");
+    }
 
-  @Test
-  public void testOptimizedDCPower() {
-    App classUnderTest = new App();
-    assertEquals(1, classUnderTest.optimizedDCPower(2, 0), "2^0 should be 1");
-    assertEquals(2, classUnderTest.optimizedDCPower(2, 1), "2^1 should be 2");
-    assertEquals(4, classUnderTest.optimizedDCPower(2, 2), "2^2 should be 4");
-    assertEquals(8, classUnderTest.optimizedDCPower(2, 3), "2^3 should be 8");
-    assertEquals(1024, classUnderTest.optimizedDCPower(2, 10), "2^10 should be 1024");
-  }
+    @Test
+    public void testUnoptimizedDCPower() {
+        App classUnderTest = new App();
+        assertEquals(1, classUnderTest.unoptimizedDCPower(2, 0), "2^0 should be 1");
+        assertEquals(2, classUnderTest.unoptimizedDCPower(2, 1), "2^1 should be 2");
+        assertEquals(4, classUnderTest.unoptimizedDCPower(2, 2), "2^2 should be 4");
+        assertEquals(8, classUnderTest.unoptimizedDCPower(2, 3), "2^3 should be 8");
+        assertEquals(1024, classUnderTest.unoptimizedDCPower(2, 10), "2^10 should be 1024");
+    }
+
+    @Test
+    public void testOptimizedDCPower() {
+        App classUnderTest = new App();
+        assertEquals(1, classUnderTest.optimizedDCPower(2, 0), "2^0 should be 1");
+        assertEquals(2, classUnderTest.optimizedDCPower(2, 1), "2^1 should be 2");
+        assertEquals(4, classUnderTest.optimizedDCPower(2, 2), "2^2 should be 4");
+        assertEquals(8, classUnderTest.optimizedDCPower(2, 3), "2^3 should be 8");
+        assertEquals(1024, classUnderTest.optimizedDCPower(2, 10), "2^10 should be 1024");
+    }
 }
